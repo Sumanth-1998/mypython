f010e61e2b893803482a7ab025f2f8b1232100c3
Sumanth <sumanth.rajesh98@gmail.com>
2019-02-18 22:33:17 +0530

    excercise 3 chap 3

---
+++ com3.py
@@ -0,0 +1,17 @@
+try:
+    score=float(input('Enter a score between 0.0 and 1.0:\n'))
+    if score<0.0 or score>1.0:
+        print('Score out of range')
+    else:
+        if score>=0.9:
+            print('A')
+        elif score>=0.8:
+            print('B')
+        elif score>=0.7:
+            print('C')
+        elif score>=0.6:
+            print('D')
+        else:
+            print('F')
+except:
+    print('Please enter valid score:')
