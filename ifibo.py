4a8a20ea5aed1359eb798f489f62360deebcba89
Sumanth <sumanth.rajesh98@gmail.com>
2019-02-20 22:07:56 +0530

    chap 4 ex 1

---
+++ ifibo.py
@@ -0,0 +1,17 @@
+def ifibo():
+    a=0
+    b=1
+    '''print(a)
+    print(b)'''
+    '''i=0'''
+    while(True):
+        '''i++'''
+        a,b=b,a+b
+        yield b
+
+        
+            
+        
+            
+
+
