87a8f0c4bdf809416d30b733472ddcf5df2de142
Sumanth <sumanth.rajesh98@gmail.com>
2019-02-20 22:13:10 +0530

    ex 1 chap 4

--- sal.py
+++ sal.py
@@ -1,1 +1,9 @@
-
+def sal(hours,rate):
+    if hours>40:
+        extra_time=float(hours)-40
+    else:
+        extra_time=0
+    extra_pay=0.5*float(rate)*extra_time
+    pay=float(hours)*float(rate)+extra_pay
+    print('pay: '),
+    print(pay)
